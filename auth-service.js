2c72de142ea9701ef5d38e5cb01d40db494f0e14
Sukhman Hara <shara1@myseneca.ca>
2024-10-23 21:43:01 -0400

    initial commit

---
+++ auth-service.js
@@ -0,0 +1,107 @@
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+let userSchema = new Schema({
+    userName: {
+        type: String,
+        unique: true
+    },
+    password: String,
+    email: String,
+    loginHistory:[{dateTime: Date, userAgent: String
+    }]
+});
+
+let User;
+
+module.exports.initialize = function ()
+{
+    return new Promise((resolve, reject) =>
+    {
+        let connect = mongoose.createConnection('mongodb+srv://shara1:<fg7sm98iMq2XpTTI>@cluster0.4ktkkpn.mongodb.net/?retryWrites=true&w=majority');
+        connect.on('error', (err) => {
+            reject(err);
+        });
+        connect.once('open', () =>
+        {
+            User = connect.model('users', userSchema);
+            resolve();
+        });
+    });
+}
+
+module.exports.registerUser = function(userData){
+    return new Promise((resolve, reject) =>
+    {
+        if (userData.password === userData.password2)
+        {
+            User.find({ userName: userData.userName }).exec()
+                .then(data =>
+                {
+                    if (data.length === 0)
+                    {
+                        let newUser = new User(userData);
+                        bcrypt.hash(newUser.password, 10).then(hash =>
+                        {
+                            newUser.password = hash
+                            newUser.save().then(() => resolve()).catch(err =>
+                                { 
+                                    if (err.code == 11000)
+                                    {
+                                        reject("Username already exists");
+                                    } else
+                                    {
+                                        reject('There was an error creating the user: ' + err);
+                                    }
+                                });
+
+                        }).catch(err => reject("There was an error encrypting the password"));
+                        
+                    } else
+                    {
+                        reject('User Name already taken');
+                    }
+                })
+        } else
+        {
+            reject('Passwords dont match');
+            }
+    })
+}
+
+module.exports.checkUser = function(userData)
+{
+    return new Promise((resolve, reject) =>
+    {
+        User.find({ userName: userData.userName }).exec()
+            .then(data =>
+            {
+                if (data.length == 0)
+                {
+                    reject('Unable to find user: ' + userData.userName)
+                }
+                bcrypt.compare(userData.password, data[0].password).then(result =>
+                {
+                    if (!result)
+                    {  
+                        reject('Incorrect Password for user ' + userData.userName)
+                    } else
+                    { 
+                        data[0].loginHistory.push({ dateTime: (new Date()).toString(), userAgent: userData.userAgent });
+                        User.updateOne(
+                            { userName: data[0].userName },
+                            { $set: { loginHistory: data[0].loginHistory } }
+                        ).exec().then(() =>
+                        {
+                            resolve(data[0]);
+                        }).catch(err => reject('There was an error verifying the user: ' + err));
+                    }
+
+                });
+                    
+            }).catch(err => 'Unable to find user ' + userData.userName);
+    });
+}
+
+
--- server.js
+++ server.js
@@ -1,14 +1,30 @@
-const express = require('express');
-const blogData = require("./blog-service");  // Correctly imported as blogData
-const multer = require("multer");
-const cloudinary = require('cloudinary').v2;
-const streamifier = require('streamifier');
-const exphbs = require("express-handlebars");
-const path = require("path");
-const stripJs = require('strip-js');
-const upload = multer();
-const app = express();
+/*********************************************************************************
+*  BTI325 – Assignment 04
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
+*  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: ____Sukhman Hara__________________ Student ID: 109790220_____________ Date: ______4/12/2023__________
+*
+*  Online (Cyclic) Link: 
+*
+********************************************************************************/ 
 
+
+const express = require('express');
+const clientSessions = require("client-sessions")
+const path = require("path");
+const app = express();
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const streamifier = require("streamifier");
+const blog_service = require("./blog-service");
+const upload = multer();
+const exphbs = require('express-handlebars');
+const stripJs = require('strip-js');
+const authData = require("./auth-service");
+
+// set port
 const HTTP_PORT = process.env.PORT || 8080;
 
 cloudinary.config({
@@ -18,25 +34,43 @@ cloudinary.config({
     secure: true
 });
 
-// Middleware
+// middleware
 app.use(express.static("public"));
-app.use(upload.single("featureImage"));
+
+app.use(
+    clientSessions({
+      cookieName: 'session', // this is the object name that will be added to 'req'
+      secret: 'o6LjQ5EVNC28ZgK64hDELM18ScpFQr', // this should be a long un-guessable string.
+      duration: 2 * 60 * 1000, // duration of the session in milliseconds (2 minutes)
+      activeDuration: 1000 * 60, // the session will be extended by this many ms each request (1 minute)
+    })
+  );
+  
 app.use(function(req, res, next) {
+    res.locals.session = req.session;
+    next();
+  });
+
+app.use(upload.single("featureImage"));
+
+
+app.use(function(req,res,next){
     let route = req.path.substring(1);
     app.locals.activeRoute = "/" + (isNaN(route.split('/')[1]) ? route.replace(/\/(?!.*)/, "") : route.replace(/\/(.*)/, ""));
     app.locals.viewingCategory = req.query.category;
     next();
 });
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
-// Handlebars engines and helpers
+
+// add handlebars engines and helpers
 app.engine('.hbs', exphbs.engine({
-    extname: '.hbs',
+    extname:'.hbs', 
     helpers: {
         navLink: function(url, options) {
-            return '<li' +
-                ((url == app.locals.activeRoute) ? ' class="active" ' : '') +
+            return '<li' + 
+                ((url == app.locals.activeRoute) ? ' class="active" ' : '') + 
                 '><a href="' + url + '">' + options.fn(this) + '</a></li>';
         },
         equal: function(lvalue, rvalue, options) {
@@ -48,56 +82,67 @@ app.engine('.hbs', exphbs.engine({
                 return options.fn(this);
             }
         },
-        safeHTML: function(context) {
+        safeHTML: function(context){
             return stripJs(context);
         },
-        formatDate: function(dateObj) {
+        formatDate: function(dateObj){
             let year = dateObj.getFullYear();
             let month = (dateObj.getMonth() + 1).toString();
             let day = dateObj.getDate().toString();
-            return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`;
+            return `${year}-${month.padStart(2, '0')}-${day.padStart(2,'0')}`;
         }
-    }
-}));
+    }}));
 app.set('view engine', 'hbs');
 
-// App routes
-app.get('/', (req, res) => {
-    res.redirect("/blog");
-});
+function ensureLogin  (req, res, next) {
+    if (!req.session.user) {
+      res.redirect('/login');
+    } else {
+      next();
+    }
+}
 
+// app routes
+app.get('/', (req, res) => {
+    res.redirect("/blog")
+});
 app.get('/about', (req, res) => {
     res.render('about');
-});
-
+})
 app.get('/blog', async (req, res) => {
     let viewData = {};
 
-    try {
+    try{
+
         let posts = [];
-        if (req.query.category) {
-            posts = await blogData.getPublishedPostsByCategory(req.query.category);
-        } else {
-            posts = await blogData.getPublishedPosts();
+
+        if(req.query.category){
+            posts = await blog_service.getPublishedPostsByCategory(req.query.category);
+        }else{
+            posts = await blog_service.getPublishedPosts();
         }
 
-        posts.sort((a, b) => new Date(b.postDate) - new Date(a.postDate));
-        let post = posts[0];
+        posts.sort((a,b) => new Date(b.postDate) - new Date(a.postDate));
+
+        let post = posts[0]; 
 
         viewData.posts = posts;
         viewData.post = post;
-    } catch (err) {
+
+    }catch(err){
         viewData.message = "no results";
     }
 
-    try {
-        let categories = await blogData.getCategories();
+    try{
+        let categories = await blog_service.getCategories();
+
         viewData.categories = categories;
-    } catch (err) {
+    }catch(err){
         viewData.categoriesMessage = "no results"
     }
 
-    res.render("blog", { data: viewData })
+    res.render("blog", {data: viewData})
+
 });
 app.get('/blog/:id', async (req, res) => {
 
@@ -140,29 +185,29 @@ app.get('/blog/:id', async (req, res) => {
 app.get('/posts', (req, res) => {
     const category = req.query.category;
     const minDate = req.query.minDate;
-
     if (category) {
-        blogData.getPostsByCategory(Number(category)) // Corrected to use blogData
-            .then((data) => res.render("posts", {posts: data}))
-            .catch((err) => res.render("posts", {message: err}));
-    } else if (minDate) {
-        blogData.getPostsByMinDate(minDate) // Corrected to use blogData
-            .then((data) => res.render("posts", {posts: data}))
-            .catch((err) => res.render("posts", {message: err}));
-    } else {
-        blogData.getAllPosts() // Corrected to use blogData
-            .then((data) => {
-                if (data.length > 0) {
-                    res.render("posts", {posts: data});
-                } else {
-                    res.render("posts", {message: "no results"});
-                }
-            })
-            .catch((err) => res.render("posts", {message: err}));
+        blog_service.getPostsByCategory(Number(category)).then((data) => res.render("posts", {posts: data}))
+        .catch((err) => res.render("posts", {message: err}));
     }
-});
+    else if (minDate) {
+        blog_service.getPostsByMinDate(minDate).then((data) => res.render("posts", {posts: data}))
+        .catch((err) => res.render("posts", {message: err}));
+    }
+    else {
+        blog_service.getAllPosts().then((data) => {
+        if (data.length > 0)
+        {
+            res.render("posts", {posts: data});
+        } else 
+        {
+            res.render("posts", { message: "no results" });
+        }
+        })
+        .catch((err) => res.render("posts", {message: err}));
+    }
+})
 app.get('/posts/add', (req, res) => {
-    blogData.getCategories().then(data => {
+    blog_service.getCategories().then(data => {
         res.render('addPost', {categories: data});
     }).catch(err => {
         res.render('addPost',{categories: []});
@@ -183,62 +228,120 @@ app.post('/posts/add', (req, res) => {
     
             streamifier.createReadStream(req.file.buffer).pipe(stream);
         });
-    }; 
+    };
     async function upload(req) {
         let result = await streamUpload(req);
         return result;
     }
     upload(req).then((uploaded)=>{
         req.body.featureImage = uploaded.url;
-        blogData.addPost(req.body)
+        blog_service.addPost(req.body)
         .then(() => {
             console.log("New post added");
             res.redirect('/posts')}); // redirect to posts
 })})
 app.get('/posts/:value', (req, res) => {
-    blogData.getPostById(Number(req.params.value)).then((data) => res.send(data))
+    blog_service.getPostById(Number(req.params.value)).then((data) => res.send(data))
     .catch((err) => {return {message: err}});
 })
 app.get('/categories', (req, res) => {
-    blogData.getCategories() // Corrected to use blogData
-        .then((data) => {
-            if (data.length > 0) {
-                res.render('categories', {categories: data});
-            } else {
-                res.render('categories', {message: "no results"});
-            }
-        })
-        .catch((err) => res.render('categories', {message: err}));
-});
+    blog_service.getCategories().then((data) => 
+    {
+        if (data.length > 0) 
+        {
+            res.render('categories', {categories:data});
+        } else 
+        {
+            res.render('categories', {message: "no results"});
+        }
+    })
+    .catch((err) => res.render('categories', {message:err}));
+})
 
 // start server if initialize is successful
-try {
-    blogData.initialize().then(() => {
-        app.listen(HTTP_PORT, () => {
-            console.log(`Express http server listening on port ${HTTP_PORT}`);
-        });
+blog_service.initialize().then(authData.initialize).then(function(){
+    app.listen(HTTP_PORT, function(){
+        console.log("app listening on: " + HTTP_PORT)
     });
-} catch (err) {
-    console.error("Initialization failed:", err);
-    throw new Error("Could not initialize data set");
-}
+}).catch(function(err){
+    console.log("unable to start server: " + err);
+});
 
 app.get('/categories/add', (req, res) => {
     res.render('addCategory');
 });
 
 app.post('/categories/add', (req,res) => {
-    blogData.addCategory(req.body).then(() => res.redirect('/categories'))
+    blog_service.addCategory(req.body).then(() => res.redirect('/categories'))
 });
 
 app.get('/categories/delete/:id', (req,res) => {
-    blogData.deleteCategoryById(req.params.id)
+    blog_service.deleteCategoryById(req.params.id)
         .then(() => res.redirect('/categories'))
         .catch(() => res.status(500).send("Unable to Remove Category / Category not found"))
 });
 
 app.get('/posts/delete/:id', (req, res) => {
-    blogData.deletePostById(req.params.id)
+    blog_service.deletePostById(req.params.id)
         .then(() => res.redirect('/posts'))
         .catch(() => res.status(500).send("Unable to Remove Category / Category not found"))
 })
+
+app.get('/login', (req, res) =>
+{
+    res.render('login');
+});
+
+app.get('/register', (req, res) =>
+{
+    res.render('register');
+});
+
+app.post('/register', (req, res) =>
+{
+    authData.registerUser(req.body).then((data) =>
+    {
+        res.render('register', { successMessage: "User created" })
+    }).catch(err =>
+    {
+        res.render('register', {errorMessage: err, userName: req.body.userName} )
+    })
+});
+
+app.post('/login', (req, res) =>
+{
+
+    req.body.userAgent = req.get('User-Agent');
+
+    authData.checkUser(req.body).then((user) => {
+        req.session.user = {
+            userName: user.userName,
+            email: user.email,
+            loginHistory: user.loginHistory
+        }
+        res.redirect('/posts');
+    }).catch(err =>
+    {
+        res.render('login', {errorMessage: err, userName: req.body.userName})
+    })
+    
+});
+
+app.get('/logout', (req, res) =>
+{
+    req.session.reset();
+    res.redirect('/');
+});
+
+app.get('/userHistory', ensureLogin, (req, res) =>
+{
+    res.render('userHistory');
+});
+  1
+app.use((req, res) => {
+  res.status(404).send("Error in my code");
+});
+
+
+
+
